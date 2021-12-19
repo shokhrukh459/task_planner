948441b7e1e3fad36672a6a20b25092a07cd830a
Shokhrukh <shokhrukh.rakhmatov1234@gmail.com>
2021-12-19 18:16:55 +0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: 200,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Container(
+                      decoration: BoxDecoration(
+
+
+                      ),
+                    ),
+                  ),
+
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
