647743e9675ef2344dd9cb73f75c66b27b83f07f
Shokhrukh <shokhrukh.rakhmatov1234@gmail.com>
2021-12-24 10:12:02 +0500

    it is ready

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class HomePage extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            backgroundColor: Colors.red.shade50,
+            backgroundColor: Colors.white,
             actions: [
               IconButton(
                 onPressed: () => {},
@@ -23,10 +24,11 @@ class HomePage extends StatelessWidget {
             ],
             pinned: true,
             expandedHeight: 240,
-            title: Text("Shokhrukh Rakhmatov",
-            style: TextStyle(
-              color: Colors.black,
-            ),
+            title: Text(
+              "Shokhrukh Rakhmatov",
+              style: TextStyle(
+                color: Colors.black,
+              ),
             ),
             flexibleSpace: FlexibleSpaceBar(
               background: Container(
@@ -82,20 +84,458 @@ class HomePage extends StatelessWidget {
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) {
+              (context, index) {
                 return Column(
                   children: [
-                    Container(
-                      height: 80,
-                      color: Colors.red.shade50,
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(child: ContentUi()),
                     ),
                   ],
                 );
               },
+              childCount: 1,
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+class ContentUi extends StatelessWidget {
+  const ContentUi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "My Tasks",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 20,
+              ),
+            ),
+            Container(
+              height: 48,
+              width: 48,
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Icon(
+                Icons.calendar_today,
+                size: 20,
+                color: Colors.white,
+              ),
+            )
+          ]),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Icon(
+              Icons.timer,
+              size: 16,
+              color: Colors.white,
+            ),
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "To Do",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                "5 tasks now / 1 started",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black.withOpacity(.2),
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      SizedBox(
+        height: 16,
+      ),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Icon(
+              Icons.ac_unit_outlined,
+              size: 16,
+              color: Colors.white,
+            ),
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "In Progress",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                "1 tasks now / 1 started",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black.withOpacity(.2),
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      SizedBox(
+        height: 16,
+      ),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Icon(
+              Icons.skip_next,
+              size: 16,
+              color: Colors.white,
+            ),
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Done",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                "18 tasks now / 18 completed",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black.withOpacity(.2),
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      SizedBox(
+        height: 16,
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+        child: Text(
+          "Active Projects",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+              height: 240,
+              width: MediaQuery.of(context).size.width * 0.42,
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Center(
+                        child: Text(
+                          "25%",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Colors.teal,
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white.withOpacity(.2),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Medical App",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "9 hours program",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          Container(
+              height: 240,
+              width: MediaQuery.of(context).size.width * 0.42,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Center(
+                        child: Text(
+                          "25%",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Colors.red,
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white.withOpacity(.2),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sport App",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "40 hours program",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ],
+      ),
+      SizedBox(height: 24,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+              height: 240,
+              width: MediaQuery.of(context).size.width * 0.42,
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Center(
+                        child: Text(
+                          "25%",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Colors.orangeAccent,
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white.withOpacity(.2),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Medical App",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "9 hours program",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          Container(
+              height: 240,
+              width: MediaQuery.of(context).size.width * 0.42,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Center(
+                        child: Text(
+                          "25%",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Colors.blueAccent,
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white.withOpacity(.2),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sport App",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "40 hours program",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ],
+      ),
+    ]);
   }
 }
